11b8629b69f17b0cdd04daa7ed7753e3f6ea3480
Benjamin Armintor <armintor@gmail.com>
2012-02-17 17:22:32 -0500

    cul_om_scv from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,30 @@ class User < ActiveRecord::Base
 # Connects this user object to Blacklights Bookmarks and Folders. 
  include Blacklight::User
  include Hydra::User
+ include Devise::Models::DatabaseAuthenticatable # this is a hack for encryptable to work
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :wind_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :encryptable,
          :authentication_keys => [:login]
-
+  wind_host "wind.columbia.edu"
+  wind_service "culscv"
   # Setup accessible (or protected) attributes for your model
   attr_accessible :login, :email, :password, :password_confirmation, :remember_me
 
   validates_uniqueness_of :login, :email, :case_sensitive => false
+
+  before_validation(:default_email, :on => :create) 
+  before_validation(:generate_password, :on => :create) 
+  before_create :set_personal_info_via_ldap
+  
+  def last_login_at
+    read_attribute(:last_sign_in_at).to_s
+  end
+  
+  def current_login_at
+    read_attribute(:current_sign_in_at).to_s
+  end
 
   def login
     read_attribute(:login).to_s
@@ -22,6 +36,40 @@ class User < ActiveRecord::Base
   # the account. 
   def to_s
     email
+  end
+  
+  def default_email
+    login = self.send User.wind_login_field
+    mail = "#{login}@columbia.edu"
+    self.email = mail
+  end
+  
+  def generate_password
+    self.password = ActiveSupport::SecureRandom.base64(8)
+  end
+  
+  def set_personal_info_via_ldap
+    if wind_login
+      entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", wind_login)) || []
+      entry = entry.first
+
+      if entry
+        _mail = entry[:mail].to_s
+        if _mail.length > 6 and _mail.match(/^[\w.]+[@][\w.]+$/)
+          self.email = _mail
+        else
+          self.email = wind_login + '@columbia.edu'
+        end
+        if User.column_names.include? :last_name
+          self.last_name = entry[:sn].to_s
+        end
+        if User.column_names.include? :first_name
+          self.first_name = entry[:givenname].to_s
+        end
+      end
+    end
+
+    return self
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Hypatia::Application.routes.draw do
   root :to => "catalog#index"
-  devise_for :users
-  resources :users, :only => :show
+  #devise_for :users, :controllers => {:sessions => 'sessions'}
 
   Blacklight.add_routes(self)
   HydraHead.add_routes(self)
+  DeviseWind.add_routes(self)
+  resources :users, :only => :show
+
   match 'catalog/:id/edit_members' => "catalog#edit_members"
   match 'catalog/:id/update_members' => "catalog#update_members"
   match 'assets/:asset_id/downloads' => "assets#download"
+
   # default routes
   match ':controller/:action/:id'
   match ':controller/:action/:id.:format'
